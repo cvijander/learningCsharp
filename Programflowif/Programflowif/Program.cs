fd4c5c841f3d312964c8cb18d1530115e3a4eb7c
cvijander <cvija85@gmail.com>
2023-08-14 23:37:36 +0200

    If else statement

---
+++ Programflowif/Programflowif/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int theVal = 51;
+
+// TODO  if-else 
+if(theVal == 50 )
+{
+    Console.WriteLine("TheVal us 50" );
+}
+else if (theVal >= 51 && theVal <= 60)
+{
+    Console.WriteLine("TheVal is between 51 and 60" );
+}
+else
+{
+    Console.WriteLine("TheVal us something else");
+}
+
+
+// -------------------
+
+// TODO Using the ternary operator ? :
+Console.WriteLine( theVal <50 ? "theVal is small" : "theVal is large" );
+
--- comments/comments/Program.cs
+++ comments/comments/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Single line comments start with two slashes 
+// you can have as many of thise as you want 
+
+Console.WriteLine("Some multiline comments"  );
+
+/* Multiple line comments starts withs  a slash and a star
+ * and can continue for several lines 
+ * until a closing star and slash are encountered */
+
+
+Console.WriteLine( "Xml documentation comments" );
+
+/// XML Comments are used to hlep provide documentation 
+/// They start with triple -slashes and have a special syntax
+/// <summary>
+/// This is the main sample application function
+/// </summary>
+/// <param name="args">An array of string arguments from the command line</param>
+/// <returns>
+/// No return value
+/// </returns>
+/// 
