fcc7b351684389a8b0a59451645a6881edeaae4b
cvijander <cvija85@gmail.com>
2023-08-14 23:44:36 +0200

    Switch case

---
+++ switch/switch/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int theVal = 50;
+
+// TODO:  The switch statement
+
+switch (theVal)
+{
+    case 50:
+        Console.WriteLine("theVal is 50");
+        break;
+
+    case 51:
+        Console.WriteLine("theVal is 51");
+        break;
+
+    case 52:
+    case 53:
+    case 54:
+        Console.WriteLine( "theVal is between 52 and 54" );
+        break;
+
+    default:
+        Console.WriteLine("theVal is something else");
+        break;
+
+}
