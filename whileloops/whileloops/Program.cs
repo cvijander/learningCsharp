c99b1bd8043d6904490bb1912bc3143a681011ab
cvijander <cvija85@gmail.com>
2023-08-15 00:05:25 +0200

    While and do while loop

---
+++ whileloops/whileloops/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string inputStr = "";
+
+// TODO basic while loop 
+
+Console.WriteLine( "Basic while loop:" );
+/*
+while ( inputStr !="exit" )
+{
+    inputStr = Console.ReadLine();
+    Console.WriteLine("You entered: {0}",inputStr );
+}
+Console.WriteLine(  );
+*/
+
+// TODO the do while loor always executes at least one time 
+Console.WriteLine( "The do while loop" );
+do {
+    inputStr = Console.ReadLine();
+    Console.WriteLine("You entered: {0}",inputStr  );
+} while ( inputStr !="exit" );
+Console.WriteLine(  );
+
