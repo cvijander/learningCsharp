161fe47fad0765828cef85631a20ef15dea30a36
cvijander <cvija85@gmail.com>
2023-08-15 00:11:20 +0200

    Break and continue

---
+++ breakcontinue/breakcontinue/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] values = { 15, 7, 12, 23, 41, 28, 9, 17, 36 };
+
+
+Console.WriteLine("Using break and continue:"  );
+foreach( var val in values )
+{
+    if(val >= 20 && val <= 29)
+    {
+        continue;
+    }
+
+    Console.WriteLine( $"Val is {val}" );
+
+  /*  if(val >= 40 )
+    {
+        break;
+    }
+  */
+}
