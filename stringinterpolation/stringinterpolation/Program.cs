10043a1e0804823a13c7074c3dd873fd148eb638
cvijander <cvija85@gmail.com>
2023-08-15 17:01:53 +0200

    String interpolation

---
+++ stringinterpolation/stringinterpolation/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Declare some variables 
+string make = "Mercedes- Benz";
+string model = "G Class";
+int year = 2020;
+float miles = 8_450.27f;
+decimal price = 60_275.0m;
+
+// TODO Output information using formatting 
+Console.WriteLine("This car is a {0} {1} {2} , with {3} miles  and cost ${4}",
+    year, make,model,miles,price);
+
+// TODO Using string interpolation
+Console.WriteLine($"This car is a {year} {make} {model}, with {miles} miles and costs {price:C2}");
+
+//TODO With inline expressions
+Console.WriteLine($"This car is a {year} {make} {model} , with {{{miles * 1.6:F2}}} km and costs {price:C2} ");
