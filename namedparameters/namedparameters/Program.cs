dcb236db462c94a7f3c0f5257e7abe77f79f6970
cvijander <cvija85@gmail.com>
2023-08-17 19:12:32 +0200

    Named and default parameters

---
+++ namedparameters/namedparameters/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// TODO: Functions can provide default values for their parameters
+void PrintWithPrefix(string theStr, string prefix="")
+{
+    Console.WriteLine($"{prefix} {theStr}");
+}
+
+// TODO: Test the default parameters
+PrintWithPrefix("Hello There! ");
+PrintWithPrefix("Hell There!", ">: ");
+
+
+// TODO: Call with named params 
+PrintWithPrefix(prefix: "% ", theStr: "Hello There!");
+
+
+
