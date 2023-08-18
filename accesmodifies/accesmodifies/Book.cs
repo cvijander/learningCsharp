8095eed010cdfcdc279f5bb95fe0cf86504e1b57
cvijander <cvija85@gmail.com>
2023-08-18 03:07:32 +0200

    access modifiers

---
+++ accesmodifies/accesmodifies/Book.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accesmodifies
+{
+    // access modifiers control how propertis and methods are accessed 
+    class Book
+    {
+        // TODO: "public" memebers and methods can be accessed by ane other 
+        // NOTE: this is *NOT* the right way yo expose internal data 
+
+        public string _name;
+
+        // TODO: "protected" memebers can only be accessed by the class or a 
+        // derived class from this one 
+
+        protected string _author;
+
+        // TODO: "private" is the default and can only be accessed by code 
+        // within the class itself 
+
+        private int _pagecount;
+
+        public Book(string name, string author, int pages)
+        {
+            _name = name;
+            _author = author;
+            _pagecount = pages;
+        }
+
+        public string GetDescription()
+        {
+            return $"{_name} by {_author}";
+
+        }
+
+        public string GetName ()
+        {
+            return _name;
+        }
+
+        public void SetName(string s)
+        {
+            _name =s;
+        }
+
+        public void SetAuthor (string s)
+        {
+            _author = s;
+        }
+
+        public void SetPageCount(int p)
+        {
+            _pagecount = p;
+        }
+
+
+    }
+}
---
+++ accesmodifies/accesmodifies/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using accesmodifies;
+
+Book b1 = new Book("War and peace", "Leo Tolstoy", 825);
+
+b1._name = "Cat in the hat";
+Console.WriteLine(b1.GetDescription());
+
+b1.SetName("Grapes of Wrath");
+b1.SetAuthor("John Steinbeck");
+b1.SetPageCount(464);
+
+Console.WriteLine(b1.GetDescription());
+
+
+
