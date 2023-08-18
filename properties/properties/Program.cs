298f359377b75bd0d9fa5cf9bafde1954814646d
cvijander <cvija85@gmail.com>
2023-08-19 00:38:19 +0200

    Properties

---
+++ properties/properties/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Create a new Book object 
+using properties;
+
+Book b1 = new Book("War and peace", "Leo Tolstoy",825);
+
+//Access a couple of properties 
+
+Console.WriteLine(b1.Name);
+Console.WriteLine(b1.Description);
+
+// Set the ISBN and Price properties 
+
+b1.ISBN = "10012352545";
+b1.Price = 24.95m;
+Console.WriteLine(b1.ISBN);
+Console.WriteLine(b1.Price);
+
+// Chenge the Name and Pagecount 
+
+b1.Name = "Crime and punishment";
+b1.Pagecount = 652;
+Console.WriteLine(b1.Description);
+Console.WriteLine(b1.Name);
+Console.WriteLine(b1.Pagecount);
